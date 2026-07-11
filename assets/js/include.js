615542d08e6d9a91d1c3e9bcd4bd09383c8807cb
TahazuS <tahamamad49@gmail.com>
2026-07-11 16:49:33 +0330

    make it with AI need to fix text

--- assets/js/include.js
+++ assets/js/include.js
@@ -4,9 +4,9 @@ async function loadComponent(id, file) {
   document.getElementById(id).innerHTML = html;
 }
 
-loadComponent("header", "./assets/components/header.html");
-loadComponent("hero", "./assets/components/hero.html");
-loadComponent("product", "./assets/components/product.html");
-loadComponent("aboutus", "./assets/components/aboutus.html");
-loadComponent("help", "./assets/components/help.html");
-loadComponent("footer", "./assets/components/footer.html");
+loadComponent("header", "../assets/components/header.html");
+loadComponent("hero", "../assets/components/hero.html");
+loadComponent("product", "../assets/components/product.html");
+loadComponent("aboutus", "../assets/components/aboutus.html");
+loadComponent("help", "../assets/components/help.html");
+loadComponent("footer", "../assets/components/footer.html");
